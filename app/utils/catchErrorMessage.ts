f228633ba841f284f991045f1c571a8bb72c4505
Silas Almeida <silasadssn@gmail.com>
2020-06-12 09:25:52 -0300

    utils

---
+++ app/utils/catchErrorMessage.ts
@@ -0,0 +1,11 @@
+export function catchErrorMessage (error: Error) {
+  if(error.message.indexOf('users_email_unique') !== -1) {
+    return 'e-mail já cadastrado!'
+  } else if(error.message.indexOf('users_ssn_unique') !== -1) {
+    return 'CPF já cadastrado!'
+  } else if(error.message.indexOf('users_ein_unique') !== -1) {
+    return 'CNPJ já cadastrado!'
+  } else if(error.message.indexOf('E_ROW_NOT_FOUND') !== -1) {
+    return 'Usuário não cadastrado!'
+  }
+}
